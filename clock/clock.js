2e6e31e6ad7fee6296b3a792970207a0c6f99f02
Mohammad Javad Vafajoo <80879917+javad-hub@users.noreply.github.com>
2021-08-01 11:33:45 +0430

    Add files via upload

---
+++ clock/clock.js
@@ -0,0 +1,7 @@
+function displayTime() {
+	var elt = document.getElementById("clock");
+	var now = new Date();
+	elt.innerHTML = now.toLocaleTimeString();
+	setTimeout(displayTime, 1000);
+}
+window.onload = displayTime;
